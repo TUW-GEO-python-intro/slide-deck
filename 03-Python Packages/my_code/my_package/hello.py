fbb931b3682991496e1ffe7355eef88c430dd6be
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2015-03-16 16:10:29 +0100

    added slides for lecture 3 and update of code formatting

---
+++ 03-Python Packages/my_code/my_package/hello.py
@@ -0,0 +1,14 @@
+print "hello world"
+
+
+def function():
+    """
+    Test function for import tests.
+    """
+    print("imported function")
+
+
+variable = "Variable in hello module"
+
+if __name__ == "__main__":
+    print("code executed when module is called directly")
